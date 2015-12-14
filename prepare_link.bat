4a0d918e6736237a5258ed5a21d953e43cfb34ff
folger <folger@originlab.com.cn>
2015-12-14 14:16:51 +0800

    prepare_link

--- prepare_link.bat
+++ prepare_link.bat
@@ -2,10 +2,14 @@
 set @folders=CustomTable ^
 Filters ^
 FitFunc ^
+OriginC ^
+Samples ^
 Themes
 
 @echo on
+pushd H:\F_C_VC32
 for %%X in (%@folders%) do (
 	rmdir %%X
 	mklink /D %%X %develop%\Origin\%%X
 )
+popd
