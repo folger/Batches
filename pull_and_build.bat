db08a9f68da2ed4da2dad2505215f9db4b9e4182
folger <folger@originlab.com.cn>
2016-07-04 10:10:14 +0800

    pull_and_build.bat

--- pull_and_build.bat
+++ pull_and_build.bat
@@ -1,5 +1,12 @@
 @echo off
 
+for /f %%x in ("%date%") do set curdate=%%x
+
+set holiday=0
+if %curdate%==Sat set holiday=1
+if %curdate%==Sun set holiday=1
+if %holiday% == 1 shutdown /s /t 30
+
 pushd %develop%
 git pull
 python "%folscode%\Python\BatchBuild\BuildCmd.py" OriginAll --platform=%1 --configuration=%2 --all-output
