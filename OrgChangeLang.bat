16ec16aa2b92f89da26aaa4de55ff768f0626c19
folger <folger@originlab.com.cn>
2016-07-18 09:30:10 +0800

    OrgChangeLang

--- OrgChangeLang.bat
+++ OrgChangeLang.bat
@@ -1,10 +1,12 @@
 @echo off
 
-choice /c EGJC /m "Language "
+set orgver=Origin 9.4D
+
+choice /c EGJC /m "Language (%orgver%)"
 
 if %errorlevel%==1 set lan=E
 if %errorlevel%==2 set lan=G
 if %errorlevel%==3 set lan=J
 if %errorlevel%==4 set lan=C
 
-reg add "HKCU\SOFTWARE\Originlab\Origin 9.4D" /v Language /t REG_SZ /d %lan% /f
+reg add "HKCU\SOFTWARE\Originlab\%orgver%" /v Language /t REG_SZ /d %lan% /f
