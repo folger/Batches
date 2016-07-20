3862fce233c43be8e99991a5c96331b027a6f221
folger <folger@originlab.com.cn>
2016-07-20 09:29:24 +0800

    pullall & pushall

--- pullall.bat
+++ pullall.bat
@@ -5,12 +5,4 @@ for /d %%X in (*) do (
 	git pull
 	popd %%X
 )
-set @folders=%home%\vimfiles ^
-C:\Python33\Lib\site-packages\folstools
-for %%X in (%@folders%) do (
-	echo Pulling %%X
-	pushd %%X
-	git pull
-	popd %%X
-)
 pause
--- pushall.bat
+++ pushall.bat
@@ -5,12 +5,4 @@ for /d %%X in (*) do (
 	git push
 	popd %%X
 )
-set @folders=%home%\vimfiles ^
-C:\Python33\Lib\site-packages\folstools
-for %%X in (%@folders%) do (
-	echo Pushing %%X
-	pushd %%X
-	git push
-	popd %%X
-)
 pause
