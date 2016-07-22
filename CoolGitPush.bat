8efc496fab9aaab9cb35243e51fd02586c998fc5
folger <folger@originlab.com.cn>
2016-07-22 14:47:32 +0800

    CoolGitPush

--- CoolGitPush.bat
+++ CoolGitPush.bat
@@ -15,12 +15,12 @@ echo Patch is generated and copy to %devcodes%
 pushd ..\%devcodes%
 
 echo Try to apply patch ...
+git pull origin master
 git am -3 --keep-cr %patchfile% > nul
 del %patchfile%
 
 if %errorlevel%==0 (
 	echo Patch is successfully applied, now push to origin ...
-	git pull --rebase origin master
 	if %errorlevel%==0 (
 		git push origin master
 		echo Done ~~~~
