f23d3c17fd32f104a70f8c4d1d50d454ea983f7d
folger <folger@originlab.com.cn>
2016-08-02 15:28:24 +0800

    SOS related

--- SOS/GetLatest.bat
+++ SOS/GetLatest.bat
@@ -3,19 +3,18 @@ setlocal enableextensions enabledelayedexpansion
 set me=%~n0
 set parent=%~dp0
 
+set server=98.118.55.12:8080
+set alias=vss
 set user=folger
 set password=""
 set sos="%ProgramFiles(x86)%\SourceOffSite\soscmd.exe"
 set log="%temp%\soslog.txt"
-set sosdir=sosdir.txt
 set marker=*********************
 
-del %log% /q /f 2>nul
-
 call :tee %marker%Getting From SOS%marker%
-call :tee Start Time [%date% %time%]
+call :tee Start [%date% %time%]
 
-for /f "tokens=*" %%x in (%sosdir%) do (
+for /f "tokens=*" %%x in (sosdir.txt) do (
 	for /f "tokens=1-3 delims=," %%a in ("%%x") do (
 		set project=%%a
 		set project=!project:ORGVER=%1!
@@ -28,14 +27,14 @@ for /f "tokens=*" %%x in (%sosdir%) do (
 		set options=%%c
 	)
 	call :tee !project! === !workdir! [!options!]
-	!sos! -command GetProject -server 98.118.55.12:8080 -name !user! -password !password! -alias vss ^
+	!sos! -command GetProject -server !server! -name !user! -password !password! -alias !alias! ^
 	-project "!project!" -workdir "!workdir!" -verbose !options! >> !log!
 )
 
 call :tee Making all files writable
 attrib -r %develop%\Origin\*.* /s > nul
 
-call :tee End Time [%date% %time%]
+call :tee End [%date% %time%]
 call :tee %marker%Done !!!%marker%
 start notepad %log%
 pause
