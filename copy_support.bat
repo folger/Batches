8defffe1d29edc3b3f20651ee3789d924bb057d5
folger <folger@originlab.com.cn>
2016-08-09 16:27:43 +0800

    copy_support

--- copy_support.bat
+++ copy_support.bat
@@ -4,5 +4,9 @@ set me=%~n0
 set parent=%~dp0
 
 for %%Y in (otp otwg otm ogs ini cnf bmp xml) do (
-	for %%X in (%develop%\Origin\*.%%Y) do (copy %%X %%~nxX) > nul
+	for %%X in (%develop%\Origin\*.%%Y) do (
+		echo %%~nxX copied
+		copy %%X %%~nxX > nul
+	)
 )
+pause
