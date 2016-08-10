27e058af0f0ea5d03a2226e1bd7b6c63a1752661
folger <folger@originlab.com.cn>
2016-08-10 21:23:11 +0800

    pullall.bat

--- pullall.bat
+++ pullall.bat
@@ -1,12 +1,28 @@
 @echo off
-setlocal enableextensions
+setlocal enableextensions enabledelayedexpansion
 set me=%~n0
 set parent=%~dp0
 
-for /d %%X in (*) do (
-	echo Pulling %%X
-	pushd %%X
+set repos="%home%\vimfiles"^
+		Batches^
+		folstools^
+		Python^
+		pk10
+
+set marker=************************
+for %%a in (%repos%) do (
+	set b=%%a
+	set c=!b::=!
+	if !b!==!c! (
+		set repo=!folscode!\%%a
+	) else (
+		set repo=%%a
+	)
+	echo !marker! !repo:"=! !marker!
+	pushd !repo!
+	git status --short
 	git pull
-	popd %%X
+	popd
 )
+echo %marker% Done %marker%
 pause
