0603082955527f126d914dd4b1870fc40ead4f0b
folger <folger@originlab.com.cn>
2016-09-02 09:48:26 +0800

    copy_rc more rc files

--- copy_rc.bat
+++ copy_rc.bat
@@ -5,13 +5,16 @@ set parent=%~dp0
 
 set entries="od.rc,Source\vc32\od" ^
 			"oErrMsg.rc,Source\vc32\oErrMsg" ^
+			"OLTmsg.rc,Source\vc32\OLTmsg" ^
 			"OKern96.rc,Source\vc32\OKern96" ^
 			"Origin50.rc,Source\vc32\Origin50" ^
 			"OUser.rc,Source\vc32\OUser" ^
 			"ODlg.rc,Source\DDK Tools\ODlg" ^
 			"ODlg8.rc,Source\DDK Tools\ODlg8" ^
-			"okUtil.rc,Source\MFC\okUtil\okUtil.rc" ^
-			"OIDE.rc,Source\MFC\oide\OIDE.rc"
+			"ODlgA.rc,Source\DDK Tools\ODlgA" ^
+			"okUtil.rc,Source\MFC\okUtil\" ^
+			"OIDE.rc,Source\MFC\oide\" ^
+			"OCntrls.rc,Source\MFC\OCntrls\"
 
 for %%a in (%entries%) do (
 	for /f "tokens=1* delims=," %%b in (%%a) do (
