d0b37dc481e3f4e9ef4f695505e0f589e95689d8
folger <folger@originlab.com.cn>
2016-09-06 09:36:30 +0800

    copy_to_fs1 %build_files% should be in %temp%

--- copy_to_fs1.bat
+++ copy_to_fs1.bat
@@ -7,7 +7,7 @@ set path=%ProgramFiles%\7-Zip;%path%
 set src=%1
 set des=\\fs1\Builds\%2\I\
 set zipdes=\\fs1\Released\ZipBuilds\%2\
-set build_files=build_files.txt
+set build_files=%temp%\build_files.txt
 
 title Copy build from %src% to %des%
 
