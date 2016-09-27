ef4207c121d11661d07b389d7d9665f0317c291d
folger <folger@originlab.com.cn>
2016-09-27 11:18:01 +0800

    copy_support.bat

--- copy_support.bat
+++ copy_support.bat
@@ -4,7 +4,7 @@ set me=%~n0
 set parent=%~dp0
 
 title Copy support files for %parent%
-for %%Y in (otp otwg otm ogs ini cnf bmp xml) do (
+for %%Y in (otp otw otm ogs ini cnf bmp xml) do (
 	for %%X in (%develop%\Origin\*.%%Y) do (
 		echo %%~nxX copied
 		copy %%X %%~nxX > nul
