ab424a963a2306ec55e5aa36b66ffa4c36cac0f1
folger <folger@originlab.com.cn>
2016-11-17 13:53:35 +0800

    copy_rc

--- copy_rc.bat
+++ copy_rc.bat
@@ -3,24 +3,29 @@ setlocal enableextensions
 set me=%~n0
 set parent=%~dp0
 
-set entries="od.rc,Source\vc32\od" ^
-			"oErrMsg.rc,Source\vc32\oErrMsg" ^
-			"OLTmsg.rc,Source\vc32\OLTmsg" ^
-			"OKern96.rc,Source\vc32\OKern96" ^
-			"Origin50.rc,Source\vc32\Origin50" ^
-			"OUser.rc,Source\vc32\OUser" ^
-			"ODlg.rc,Source\DDK Tools\ODlg" ^
-			"ODlg8.rc,Source\DDK Tools\ODlg8" ^
-			"ODlgA.rc,Source\DDK Tools\ODlgA" ^
-			"okUtil.rc,Source\MFC\okUtil\" ^
-			"OIDE.rc,Source\MFC\oide\" ^
-			"OCntrls.rc,Source\MFC\OCntrls\"
+set entries="od.rc,vc32\od" ^
+			"oErrMsg.rc,vc32\oErrMsg" ^
+			"OLTmsg.rc,vc32\OLTmsg" ^
+			"OKern96.rc,vc32\OKern96" ^
+			"Origin50.rc,vc32\Origin50" ^
+			"OUser.rc,vc32\OUser" ^
+			"ODlg.rc,DDK Tools\ODlg" ^
+			"ODlg8.rc,DDK Tools\ODlg8" ^
+			"ODlgA.rc,DDK Tools\ODlgA" ^
+			"okUtil.rc,MFC\okUtil" ^
+			"OIDE.rc,MFC\oide" ^
+			"OCntrls.rc,MFC\OCntrls" ^
+			"ohttp.rc,Module\ohttp" ^
+			"Import Wizard.rc,DDK Tools\ImportWiz" ^
+			"Tab_Test.rc,DDK Tools" ^
+			"oimg.rc,Module\oimg" ^
+			"nlsf_wizard.rc,DDK Tools\NLSFWiz"
 
 for %%a in (%entries%) do (
 	for /f "tokens=1* delims=," %%b in (%%a) do (
 		if exist %%b (
 			echo %%b === %%c 
-			copy %%b "%develop%\%%c" > nul
+			copy "%%b" "%develop%\Source\%%c" > nul
 		)
 	)
 )
