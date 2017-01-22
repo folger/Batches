a607f6b1183c2f875415f7885a82fcab6dba23ca
folger <folger@originlab.com.cn>
2017-01-22 15:21:11 +0800

    Polish Build-Release.ps1

--- ps1/Build-Release.ps1
+++ ps1/Build-Release.ps1
@@ -1,23 +1,22 @@
-﻿$Json = (Get-Content "$((Get-Item $MyInvocation.MyCommand.Name).BaseName).json") -Join "`n" | ConvertFrom-Json
+$Json = (Get-Content "$((Get-Item $MyInvocation.MyCommand.Name).BaseName).json") -join "`n" | ConvertFrom-Json
 
 $ComputerName = $Json.ComputerName
 $Credential = $Json.Credential
 $BuildBat = $Json.BuildBat
 $Versions = $Json.Versions
-
-$ValidVersion = $Versions.PSObject.Properties | Select-Object -ExpandProperty Name
+$ValidVersions = $Versions.PSObject.Properties.Name
 
 $Host.ui.RawUI.WindowTitle = "Build Release on '$ComputerName'"
 
 "Conneting to $ComputerName as $Credential ..."
 $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
 
-$Version = $Versions."$($ValidVersion[0])"
+$Version = $Versions."$($ValidVersions[0])"
 while ($True) {
   $DevFolder = $Version.Path
   $Cmd = Read-Host -Prompt "[$ComputerName]($DevFolder)"
   if ($Cmd.Length -eq 0) {break}
-  if ($Cmd -in $ValidVersion) {
+  if ($Cmd -in $ValidVersions) {
     $Version = $Versions."$Cmd"
     continue
   }
