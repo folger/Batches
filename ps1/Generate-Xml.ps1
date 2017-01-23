2c0cb30066293632e8aac72cfdd329dfcfcbf58b
folger <folger@originlab.com.cn>
2017-01-23 11:34:02 +0800

    Generate-Xml.ps1

---
+++ ps1/Generate-Xml.ps1
@@ -0,0 +1,15 @@
+Get-ChildItem -Directory | Foreach-Object -Process {
+    $path = $_.FullName
+    $path
+    $json = (Get-Content "$path\Graphing.json") | ConvertFrom-Json
+    $ProjectName = $json.Projects[0].ProjectName
+
+    [xml]$doc = '<Root></Root>'
+    $root = $doc.SelectSingleNode('Root')
+    foreach ($lang in @('G', 'J', 'C')) {
+        $LangNode = $root.AppendChild($doc.CreateElement($lang))
+        $ProjectNameNode = $LangNode.AppendChild($doc.CreateElement('ProjectName'))
+        [void]$ProjectNameNode.AppendChild($doc.CreateTextNode($ProjectName))
+    }
+    $doc.Save("$path\Localization.xml")
+}
