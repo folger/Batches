9e4e4d944de14b0ba78a88b39fa56b892efe5144
folger <folger@originlab.com.cn>
2017-01-24 15:34:17 +0800

    Browse-Folder.ps1 make title as parameter

--- ps1/Browse-Folder.ps1
+++ ps1/Browse-Folder.ps1
@@ -1,19 +1,20 @@
 [CmdletBinding()]
-param()
+param(
+    [string]$Title
+)
 
-$title = "Select App to work"
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     Add-Type -AssemblyName System.Windows.Forms
     $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
-        Description = $title
-        SelectedPath = $pSScriptRoot
+        Description = $Title
+        SelectedPath = $PSScriptRoot
     }
     if ($folderBrowser.ShowDialog() -eq "OK") {
         $folderBrowser.SelectedPath
     }
 }
 else {
-    $app = New-Object -com Shell.Application
-    $folder = $app.BrowseForFolder(0, $title, 0, "")
+    $app = New-Object -Com Shell.Application
+    $folder = $app.BrowseForFolder(0, $Title, 0, "")
     $folder.Self.Path
 }
