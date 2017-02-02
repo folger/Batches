5cb8457c20e014c062e053476e625949a321baf1
folger <folger@originlab.com.cn>
2017-02-02 20:44:38 +0800

    Move ps1 as individual repo

--- ps1/Convert-Utf8.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param()
-
-Get-ChildItem -Path . -Include *.h,*.c,*.cpp -Recurse | Foreach-Object -Process {
-    $s = Get-Content -Path $_
-    $s | Out-File -Encoding UTF8 -FilePath $_
-    break
-}
