53574e358f7efcd63564280a8fe2f8198c008e33
folger <folger@originlab.com.cn>
2017-06-22 09:51:52 +0800

    GetDlls_x64

---
+++ GetDlls_x64.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+set me=%~n0
+set parent=%~dp0
+
+for /f "delims=" %%i in ('dir Origin9*.exe /b /t:w /od') do set origin=%%i
+:found_origin
+if [%origin%]==[] (
+	echo Origin executable cannot be found^^!^^!^^!
+	exit /b 1
+)
+
+set version=%origin:Origin=%
+set version=%version:.exe=%
+set version=%version:_64=%
+
+set platform=%me:GetDlls_=%
+
+xcopy \\fs1\Dev\%version%_dlls\%platform% . /s /y
+pause
