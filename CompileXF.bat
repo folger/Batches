69935d5f0d2c719e4ccc49d03da9768e5faf2046
folger <folger@originlab.com.cn>
2019-06-26 16:39:38 +0800

    Compile*.bat

--- CompileXF.bat
+++ CompileXF.bat
@@ -2,17 +2,22 @@
 setlocal enableextensions enabledelayedexpansion
 set me=%~n0
 set parent=%~dp0
+set pscmd=powershell -ExecutionPolicy Bypass -Command
 
-title Compile X-Functions
 
-for /f "delims=" %%i in ('dir Origin9*.exe /b /t:w /od') do set origin=%%i
+for /f "delims=" %%i in ('dir Origin*.exe /b /t:w /od') do set origin=%%i
 :found_origin
 if [%origin%]==[] (
 	echo Origin executable cannot be found^^!^^!^^!
 	exit /b 1
 )
 
-set version=%origin:~6,2%
+set GCI="&{Get-ChildItem 'Origin*.exe'|Sort-Object LastWriteTime -Descending}"
+for /f "usebackq delims=" %%i in (`%pscmd% ^(%GCI%^)[0].VersionInfo.ProductVersion.Replace^('.'^, ''^)`) do set version=%%i
+for /f "usebackq delims=" %%i in (`%pscmd% if ^(^(%GCI%^)[0].VersionInfo.FileDescription -match 'Origin \d+^([a-z]^)'^) {$matches[1]}`) do set version=%version%%%i
+for /f "usebackq delims=" %%i in (`%pscmd% if ^(^(%GCI%^)[0].VersionInfo.IsDebug^) {'D'} else {''}`) do set version=%version%%%i
+
+title Compile %version% X-Functions
 
 echo Found %origin% ...
 
